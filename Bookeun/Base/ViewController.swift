cc64b7030da36f0152859f928ba15942e4172d20
이병찬 <galaxy000213@gmail.com>
2019-11-21 23:10:54 +0900

    Base ViewController 추가

--- Bookeun/Base/ViewController.swift
+++ Bookeun/Base/ViewController.swift
@@ -9,11 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        attribute()
+        layout()
         
         view.backgroundColor = .white
     }
+    
+    func attribute() {}
+    
+    func layout() {}
 }
