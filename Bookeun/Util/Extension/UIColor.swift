2e643de1b6eec6da731d2d3054248c4f4a1a49ef
이병찬 <galaxy000213@gmail.com>
2019-11-24 05:50:42 +0900

    FEAT: `ColorSet` 추가

--- Bookeun/Util/Extension/UIColor.swift
+++ Bookeun/Util/Extension/UIColor.swift
@@ -6,4 +6,62 @@
 //  Copyright © 2019 Lizardmon. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    @nonobjc class var greyishBrown: UIColor {
+      return UIColor(white: 83.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var weirdGreen: UIColor {
+      return UIColor(red: 76.0 / 255.0, green: 217.0 / 255.0, blue: 100.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var paleLilac: UIColor {
+      return UIColor(red: 229.0 / 255.0, green: 229.0 / 255.0, blue: 234.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var paleGrey: UIColor {
+      return UIColor(red: 239.0 / 255.0, green: 239.0 / 255.0, blue: 244.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var lightBlueGrey: UIColor {
+      return UIColor(red: 209.0 / 255.0, green: 209.0 / 255.0, blue: 214.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var lightBlueGreyTwo: UIColor {
+      return UIColor(red: 199.0 / 255.0, green: 199.0 / 255.0, blue: 204.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var blueGrey: UIColor {
+      return UIColor(red: 142.0 / 255.0, green: 142.0 / 255.0, blue: 147.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var ice: UIColor {
+      return UIColor(red: 235.0 / 255.0, green: 251.0 / 255.0, blue: 238.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var paleGreyTwo: UIColor {
+      return UIColor(red: 248.0 / 255.0, green: 249.0 / 255.0, blue: 250.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var iceTwo: UIColor {
+      return UIColor(red: 231.0 / 255.0, green: 252.0 / 255.0, blue: 237.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var tealishGreen: UIColor {
+      return UIColor(red: 9.0 / 255.0, green: 222.0 / 255.0, blue: 141.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var brightSeaGreen: UIColor {
+      return UIColor(red: 0.0, green: 1.0, blue: 157.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var veryLightPink: UIColor {
+      return UIColor(white: 224.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var veryLightPinkTwo: UIColor {
+      return UIColor(white: 215.0 / 255.0, alpha: 1.0)
+    }
+}
