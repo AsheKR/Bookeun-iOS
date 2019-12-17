94e7c9db017f43ed2e6dfd6172dcdb548746f896
onemoon <onemoonstudio@gmail.com>
2019-12-17 17:18:32 +0900

    Merge branch 'develop' into feature/6_exercise_UI
    # Conflicts:
    #	Bookeun.xcodeproj/project.pbxproj
    #	Bookeun/RootWindow.swift

--- Bookeun/RootWindow.swift
+++ Bookeun/RootWindow.swift
@@ -11,17 +11,24 @@ import UIKit
 class RootWindow: UIWindow {
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        self.rootViewController = EmptyViewController()
-        
-        Repo.shared.exercise.getExerciseList()
-            .map { list in
-                list.map { exc in ExerciseWithCount.init(exercise: exc, count: 3) }
-            }
-            .subscribe(onSuccess: { [unowned self] list in
-                Store.share.setExerciseList(list)
-                self.rootViewController = UIStoryboard(name: "ExerciseViewController", bundle: nil).instantiateInitialViewController() as! ExerciseViewController
-            })
+//<<<<<<< HEAD
+//
+//        self.rootViewController = EmptyViewController()
+//
+//        Repo.shared.exercise.getExerciseList()
+//            .map { list in
+//                list.map { exc in ExerciseWithCount.init(exercise: exc, count: 3) }
+//            }
+//            .subscribe(onSuccess: { [unowned self] list in
+//                Store.share.setExerciseList(list)
+//                self.rootViewController = UIStoryboard(name: "ExerciseViewController", bundle: nil).instantiateInitialViewController() as! ExerciseViewController
+//            })
+//=======
+
+        // TODO: 초기뷰 변경 필요!
+        let viewController = UIStoryboard(name: SelectExerciseViewController.storyboardName, bundle: nil).instantiateViewController(withIdentifier: SelectExerciseViewController.identifier)
+        let navigation = UINavigationController(rootViewController: viewController)
+        self.rootViewController = navigation
     }
     
     required init?(coder: NSCoder) {
