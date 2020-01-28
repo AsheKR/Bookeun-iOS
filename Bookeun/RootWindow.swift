71c9f2ccd43345e2bb811eaf439554914db47050
onemoon <onemoonstudio@gmail.com>
2020-01-28 21:24:40 +0900

    REFACTOR : code review

--- Bookeun/RootWindow.swift
+++ Bookeun/RootWindow.swift
@@ -12,15 +12,7 @@ class RootWindow: UIWindow {
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.rootViewController = TrainerSelectViewController()
-//        self.rootViewController = BarcodeViewController()
-        Repo.shared.exercise.getExerciseList().map { exerciseList in
-            exerciseList.map { exercise in
-                ExerciseWithCount(exercise: exercise, count: 3)
-            }
-        }.subscribe(onSuccess: { exerciseWithCount in
-            Store.share.setExerciseList(exerciseWithCount)
-            self.rootViewController = UIStoryboard(name: ExerciseViewController.identifier, bundle: nil).instantiateInitialViewController()!
-        })
+        self.rootViewController = BarcodeViewController()
     }
     
     required init?(coder: NSCoder) {
